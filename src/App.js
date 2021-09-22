51350ffed74ae0fc8ba7eb0146d56951e4cc9ddc
fumzy <you@example.comnanya.loveth@gmail.com>
2021-09-22 10:20:38 +0100

    updated menu-item component to use withRouter component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 import HomePage from './pages/homepage/homepage.component';
+import './App.css';
+
+const WigsPage = ()=> (
+  <div>
+  <h1>Wigs Page</h1>
+  </div>
+)
 
 function App() {
   return (
     <div>
-   <HomePage />
+    <Switch>
+    <Route exact={true} path='/' component={HomePage} />
+    <Route  path='/wigs' component={WigsPage} />
+  </Switch>
+   
+  
     </div>
   );
 }
